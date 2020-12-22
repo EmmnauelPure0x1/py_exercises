ce67cbd27d99e16f74c219e8b320aaacf1fe3606
EmmanuelPure0x1 <eadejobi@spartaglobal.com>
2020-12-22 16:00:51 +0000

    text file - list

--- read_file.py
+++ read_file.py
@@ -2,11 +2,18 @@
 
 print("++++++++++")
 
+# opening text document and adding 'read' powers. Storing this action into text variable
 text = open('text.txt', 'r')
 
+# looping over each line in the text file and printing it capitalized.
 for line in text:
     print(line.title())
 
+# var which equals opened document is closed (best practice)
 text.close()
 
 print("++++++++++")
+
+t2 = open('text_file_2.txt', 'r')
+print(t2.readlines())
+t2.close()
